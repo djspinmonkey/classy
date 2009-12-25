115c46ac9532957305d7cc01899b72568db7bd79
John Hyland <github@djspinmonkey.com>
2009-12-25 15:30:42 -0500

    significantly fleshed out rdoc

--- lib/classy/subclass_aware.rb
+++ lib/classy/subclass_aware.rb
@@ -1,15 +1,55 @@
 require 'set'
 
+# SubclassAware allows a class to know about all of the subclasses that descend
+# from it in the inheritance tree.
+#
+# Example:
+#
+#   class Parent
+#     extend SubclassAware
+#   end
+#
+#   class ChildA < Parent
+#   end
+#
+#   class ChildB < Parent
+#   end
+#
+#   Class ChildB1 < ChildB
+#   end
+#
+#   Parent.subclasses   # => [ ChildA, ChildB, ChildB1 ]
+#
+# Warning: This module defines an inherited() class method on the extending
+# class to keep track of subclasses.  Unfortunately, if this method is later
+# re-defined, this inherited() method is lost and subclass tracking will break.
+# In order to work around this, constructions like the following might be
+# necessary:
+#
+#   class ChildC < Parent
+#
+#     class << self; alias :old_inherited :inherited end
+#     def self.inherited(sub)
+#       old_inherited(sub)
+#       # ...your inherited() code...
+#     end
+#
+#   end
+#
+# This is not considered an acceptable long-term state of affairs - hopefully
+# in future versions of this module, this work around will not be necessary.
+#
 module SubclassAware
 
-  def self.extended (klass) #nodoc;
+  def self.extended (klass) #:nodoc:
     klass.class_exec { class_variable_set(:@@subclasses, Set.new) }
   end
 
   # TODO: Find a way for self.inherited on the extended class not to blow
   # this away without requiring a bunch of alias chain hoops to be jumped
   # through.
-  def inherited(sub) #nodoc;
+  #
+  def inherited(sub) #:nodoc:
     class_exec { class_variable_get(:@@subclasses).add sub }
   end
 
@@ -20,8 +60,8 @@ module SubclassAware
     class_exec { class_variable_get(:@@subclasses).to_a }
   end
 
-  # Clear all info about known subclasses.  Usefull for testing, but it is
-  # unlikely you would use it for much else.
+  # Clear all info about known subclasses.  This method is probably mainly
+  # useful for testing.
   #
   def forget_subclasses
     class_exec { class_variable_get(:@@subclasses).clear }
